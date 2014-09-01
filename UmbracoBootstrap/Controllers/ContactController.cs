9960f2b426cfc3270bf9300d9c488af2d43792f4
Vincent Yang <vincent.yang@orchard.com.au>
2014-09-01 19:09:53 +1000

    Add Contact Us Form

---
+++ UmbracoBootstrap/Controllers/ContactController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using UmbracoBootstrap.Models;
+
+namespace UmbracoBootstrap.Controllers
+{
+    public class ContactController : Umbraco.Web.Mvc.SurfaceController
+    {
+        [HttpPost]
+        public ActionResult SendMail(Contact form)
+        {
+
+            string retValue = "There was an error submitting the form, please try again later.";
+            if (!ModelState.IsValid)
+            {
+                return Content(retValue);
+            }
+
+            if (ModelState.IsValid)
+            {
+                //Update your SMTP server credentials
+                using (var client = new SmtpClient
+                {
+                    Host = "smtp.orchard.net.au"                    
+                })
+                {
+                    var mail = new MailMessage();
+                    mail.To.Add("vincent.yang@orchard.com.au"); // Update your email address
+                    mail.From = new MailAddress(form.Email, form.Name);
+                    mail.Subject = String.Format("Request to Contact from {0}", form.CompanyName);
+                    mail.Body = form.Message;
+                    mail.IsBodyHtml = false;
+                    try
+                    {
+                        client.Send(mail);
+                        retValue = "Your Request for Contact was submitted successfully. We will contact you shortly.";
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+            return Content(retValue);
+        }
+    }
+}
---
+++ UmbracoBootstrap/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UmbracoBootstrap.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+        
+        [Required]
+        public string Message { get; set; }
+    }
+}
